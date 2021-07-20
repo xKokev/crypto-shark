7ac2b12ce253d5f82e02868459729a8c97e677d1
Kevin <10kevinho@gmail.com>
2021-07-20 16:51:35 -0400

    feat: :zap: 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import imagen from './cryptomonedas.png';
+import Formulario from './components/Formulario';
+import Cotizacion from './components/Cotizacion';
+import Spinner from './components/Spinner';
+import styled from '@emotion/styled';
+
 const Contenedor = styled.div`
   max-width: 900px;
   margin: 0 auto;
@@ -15,7 +20,6 @@ const Imagen = styled.img`
   max-width: 100%;
   margin-top: 5rem;
 `;
-
 const Heading = styled.h1`
   font-family: 'Bebas Neue', cursive;
   color: #fff;
@@ -34,13 +38,57 @@ const Heading = styled.h1`
 `;
 
 function App() {
+  const [moneda, guardarMoneda] = useState('');
+  const [criptomoneda, guardarCriptomoneda] = useState('');
+  const [resultado, guardarResultado] = useState({});
+  const [cargando, guardarCargando] = useState(false);
+
+  useEffect(() => {
+    const cotizarCriptomoneda = async () => {
+      // evitamos la ejecución la primera vez
+      if (moneda === '') return;
+
+      // consultar la api para obtener la cotizacion
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`;
+
+      const resultado = await axios.get(url);
+
+      // mostrar el spinner
+      guardarCargando(true);
+
+      // ocultar el spinner y mostrar el resultado
+      setTimeout(() => {
+        // cambiar el estado de cargando
+        guardarCargando(false);
+
+        // guardar cotizacion
+        guardarResultado(resultado.data.DISPLAY[criptomoneda][moneda]);
+      }, 3000);
+    };
+    cotizarCriptomoneda();
+  }, [moneda, criptomoneda]);
+
+  // Mostrar spinner o resultado
+  const componente = cargando ? (
+    <Spinner />
+  ) : (
+    <Cotizacion resultado={resultado} />
+  );
+
   return (
     <Contenedor>
       <div>
         <Imagen src={imagen} alt='imagen cripto' />
       </div>
       <div>
-        <Heading>Cotiza criptomoneda al instante </Heading>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+
+        <Formulario
+          guardarMoneda={guardarMoneda}
+          guardarCriptomoneda={guardarCriptomoneda}
+        />
+
+        {componente}
       </div>
     </Contenedor>
   );
